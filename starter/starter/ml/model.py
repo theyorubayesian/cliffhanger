ba7dce141db2855c89b3bd6f2249bde623f996f2
Akintunde Oladipo <akin.o.oladipo@gmail.com>
2021-06-18 06:08:07 +0100

    refactor: dvc commits individual files not dir

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.linear_model import LogisticRegression
 
 
 # Optional: implement hyperparameter tuning.
@@ -17,8 +18,9 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-
-    pass
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +59,4 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    return model.predict(X)
