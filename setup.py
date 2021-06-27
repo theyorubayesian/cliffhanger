a39d940a3b2958525ccf976e2a5b0d080c87c77f
Akintunde Oladipo <akin.o.oladipo@gmail.com>
2021-06-27 02:48:14 +0100

    minor fixes

---
+++ setup.py
@@ -0,0 +1,12 @@
+# from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="cliffhanger",
+    version="0.0.0",
+    description="ML Classification API",
+    author="theyorubayesian",
+    author_email="akin.o.oladipo@gmail.com",
+    url="https://github.com/theyorubayesian/cliffhanger",
+    # install_requires=find_packages(include=['cliffhanger'])
+)
